eaa0fc8bef136439df7188aec49a669e55ce90d7
Jacob Lockhart <procoder165@gmail.com>
2024-10-25 13:57:19 -0400

    Added Break cases and price formatting

--- car-workshop/src/main/java/com/pluralsight/cars/UserInterface.java
+++ car-workshop/src/main/java/com/pluralsight/cars/UserInterface.java
@@ -3,6 +3,7 @@ package com.pluralsight.cars;
 import com.pluralsight.cars.JavaHelpers.ColorCodes;
 
 import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.Scanner;
 
@@ -83,32 +84,33 @@ public class UserInterface {
 
     public String displayVehicles(List<Vehicle> listOfVehicle) {
         StringBuilder sb = new StringBuilder();
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
         for (Vehicle vehicle : listOfVehicle) {
-            String make = String.format(ColorCodes.YELLOW + "  %-15s ", vehicle.getMake() + ColorCodes.RESET);
+            String make = String.format(ColorCodes.YELLOW + "  %-16s ", vehicle.getMake() + ColorCodes.RESET);
             String model = String.format(ColorCodes.GREEN + " %-14s ", vehicle.getModel() + ColorCodes.RESET);
             String year = String.format(ColorCodes.BLUE + "      %-14s ", vehicle.getYear() + ColorCodes.RESET);
             String color = String.format(ColorCodes.RED + " %-18s ", vehicle.getColor() + ColorCodes.RESET);
             String mileage = String.format(ColorCodes.PURPLE + " %-14s ", vehicle.getOdometer() + ColorCodes.RESET);
-            String price = String.format(ColorCodes.WHITE + " %-10.2f ", vehicle.getPrice());
+            String price = String.format(ColorCodes.WHITE + " %-14s ",nf.format(vehicle.getPrice()));
 
             if (listOfVehicle.size() == 1) {
-                sb.append("╔════════════╦══════════════╦════════════╦══════════════╦═════════════╦═════════════╗\n");
-                sb.append("║    Make:   ║   Model:     ║    Year:   ║    Color:    ║   Mileage:  ║   Price:    ║\n");
-                sb.append("╠════════════╩══════════════╩════════════╩══════════════╩═════════════╩═════════════╣\n");
+                sb.append("╔═════════════╦══════════════╦════════════╦══════════════╦═════════════╦═════════════════╗\n");
+                sb.append("║    Make:    ║   Model:     ║    Year:   ║    Color:    ║   Mileage:  ║   Price:        ║\n");
+                sb.append("╠═════════════╩══════════════╩════════════╩══════════════╩═════════════╩═════════════════╣\n");
                 sb.append("║").append(make).append(model).append(year).append(color).append(mileage).append(price).append(ColorCodes.RESET).append("║").append("\n");
-                sb.append("╚═══════════════════════════════════════════════════════════════════════════════════╝\n");
+                sb.append("╚════════════════════════════════════════════════════════════════════════════════════════╝\n");
             } else if (listOfVehicle.get(0) == vehicle) {
 
 
-                sb.append("╔════════════╦══════════════╦════════════╦══════════════╦═════════════╦═════════════╗\n");
-                sb.append("║    Make:   ║   Model:     ║    Year:   ║    Color:    ║   Mileage:  ║   Price:    ║\n");
-                sb.append("╠════════════╩══════════════╩════════════╩══════════════╩═════════════╩═════════════╣\n");
+                sb.append("╔═════════════╦══════════════╦════════════╦══════════════╦═════════════╦═════════════════╗\n");
+                sb.append("║    Make:    ║   Model:     ║    Year:   ║    Color:    ║   Mileage:  ║   Price:        ║\n");
+                sb.append("╠═════════════╩══════════════╩════════════╩══════════════╩═════════════╩═════════════════╣\n");
                 sb.append("║").append(make).append(model).append(year).append(color).append(mileage).append(price).append(ColorCodes.RESET).append("║").append("\n");
 
 
             } else if (listOfVehicle.get(listOfVehicle.size() - 1) == vehicle) {
                 sb.append("║").append(make).append(model).append(year).append(color).append(mileage).append(price).append(ColorCodes.RESET).append("║").append("\n");
-                sb.append("╚═══════════════════════════════════════════════════════════════════════════════════╝\n");
+                sb.append("╚════════════════════════════════════════════════════════════════════════════════════════╝\n");
 
                 return sb.toString();
             } else {
@@ -128,27 +130,45 @@ public class UserInterface {
         int minNumber = 0;
         int maxNumber = 0;
         while (userMinNaN) {
-            System.out.println("What is the minimum " + prompt + " you are looking for?");
+            System.out.println("What is the minimum " + prompt + " you are looking for? (Enter 'x' to break)");
             String min = scan.nextLine();
             try {
+                if(min.equalsIgnoreCase("x")){
+                    return new int[] {-1, -1};
+                }
                 minNumber = Integer.parseInt(min);
+                if(minNumber < 0){
+                    System.out.println("Please enter a positive number");
+                }
+                else{
                 userMinNaN = false;
+                }
             } catch (NumberFormatException e) {
                 System.out.println("That is not a number");
             }
         }
         while (userMaxNaN) {
-            System.out.println("What is the maximum " + prompt + " you are looking for?");
+            System.out.println("What is the maximum " + prompt + " you are looking for? (Enter 'x' to break)");
             try {
                 String max = scan.nextLine();
+                if(max.equalsIgnoreCase("x")){
+                    return new int[] {-1, -1};
+                }
                 maxNumber = Integer.parseInt(max);
-                userMaxNaN = false;
+                if(maxNumber < 0){
+                    System.out.println("Please enter a positive number");
+                }
+                else{
+                    userMaxNaN = false;
+                }
+
             } catch (NumberFormatException e) {
                 System.out.println("That is not a number");
             }
         }
         return new int[]{minNumber, maxNumber};
     }
+
 
     public void processGetByPriceRequest() {
         int[] userRange = getUserRange("price");
@@ -157,8 +177,12 @@ public class UserInterface {
 
     public void processGetByMakeModelRequest() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("What is the make and the model of the car?");
-        String[] makeModel = scan.nextLine().trim().split(" ");
+        System.out.println("What is the make and the model of the car? (Enter 'x' to exit)");
+        String userInput = scan.nextLine();
+        if(userInput.equalsIgnoreCase("x")){
+            return;
+        }
+        String[] makeModel = userInput.trim().split(" ");
 
         System.out.println(displayVehicles(dealership.getVehiclesByMakeModel(makeModel[0], makeModel[1])));
     }
@@ -170,8 +194,11 @@ public class UserInterface {
 
     public void processGetByColorRequest() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("What is the color of the car?");
+        System.out.println("What is the color of the car? (Enter 'x' to exit)");
         String color = scan.nextLine();
+        if(color.equalsIgnoreCase("x")){
+            return;
+        }
         System.out.println(displayVehicles(dealership.getVehiclesByColor(color)));
     }
 
@@ -182,8 +209,11 @@ public class UserInterface {
 
     public void processGetByTypeRequest() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("What is the vehicle type? (car, SUV, Truck, van)");
+        System.out.println("What is the vehicle type? (car, SUV, Truck, Van, Hypercar) (Enter 'x' to exit)");
         String type = scan.nextLine();
+        if(type.equalsIgnoreCase("x")){
+            return;
+        }
 
         System.out.println(displayVehicles(dealership.getVehiclesByType(type)));
     }
